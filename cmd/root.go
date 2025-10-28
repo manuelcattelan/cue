0e1a3581a70d34b1b99d4012cd3e5b18f6ff810f
Manuel Cattelan <cattelanmanuel01@gmail.com>
2025-10-28 08:18:33 +0100

    chore!: cleanup and handle configuration init

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,42 @@
 package cmd
 
 import (
+	"errors"
 	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+var rootConfig string
 var rootCommand = &cobra.Command{
 	Use: "cue",
-	Short: "CLI-first tool that generates LLM-ready prompts" +
+	Short: "CLI-first tool that generates LLM-ready prompts " +
 		"optimized to get most out of your agentic buddy.",
+
 	PersistentPreRunE: func(command *cobra.Command, arguments []string) error {
 		viper.SetEnvPrefix("CUE")
 		viper.AutomaticEnv()
+
+		if rootConfig != "" {
+			viper.SetConfigFile(rootConfig)
+		} else {
+			userConfigDirectory, userConfigDirectoryError := os.UserConfigDir()
+			cobra.CheckErr(userConfigDirectoryError)
+
+			viper.AddConfigPath(".")
+			viper.AddConfigPath(userConfigDirectory + "/cue")
+
+			viper.SetConfigName("config")
+			viper.SetConfigType("yaml")
+		}
+
+		if readInConfigError := viper.ReadInConfig(); readInConfigError != nil {
+			var configFileNotFoundError viper.ConfigFileNotFoundError
+			if !errors.As(readInConfigError, &configFileNotFoundError) {
+				return readInConfigError
+			}
+		}
 
 		bindFlagsError := viper.BindPFlags(command.Flags())
 		if bindFlagsError != nil {
@@ -22,6 +45,8 @@ var rootCommand = &cobra.Command{
 
 		return nil
 	},
+
+	Run: func(command *cobra.Command, arguments []string) {},
 }
 
 func Execute() {
@@ -31,4 +56,7 @@ func Execute() {
 	}
 }
 
-func init() {}
+func init() {
+	rootCommand.PersistentFlags().StringVar(&rootConfig, "config", "",
+		"configuration file")
+}
