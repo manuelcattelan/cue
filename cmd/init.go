6a1e632208edb902f27432538cdda2da5a5039c0
Manuel Cattelan <cattelanmanuel01@gmail.com>
2025-10-30 09:20:48 +0100

    feat: configuration init form with provider information

--- cmd/init.go
+++ cmd/init.go
@@ -1,63 +1,139 @@
 package cmd
 
 import (
-	"fmt"
+	"cue/internal/config"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-
-	"cue/internal/config"
-)
-
-const (
-	ConfigKey = "key"
 )
 
 type Model struct {
-	textInput      textinput.Model
-	valueSubmitted bool
-	value          string
-}
+	providerID          int
+	providerIDSubmitted bool
 
-func initialModel() Model {
-	textInput := textinput.New()
-	textInput.Focus()
+	providerModel          int
+	providerModelSubmitted bool
 
-	return Model{
-		textInput: textInput,
-	}
+	providerAPIKeyTextInput textinput.Model
+	providerAPIKey          string
+	providerAPIKeySubmitted bool
 }
 
 func (model Model) Init() tea.Cmd {
 	return nil
 }
 
-func (model Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
-	var command tea.Cmd
+func initialModel() Model {
+	return Model{
+		providerID:          0,
+		providerIDSubmitted: false,
 
-	switch messageType := message.(type) {
+		providerModel:          0,
+		providerModelSubmitted: false,
+
+		providerAPIKeyTextInput: textinput.New(),
+		providerAPIKey:          "",
+		providerAPIKeySubmitted: false,
+	}
+}
+
+func (model Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+	switch typeMessage := message.(type) {
 	case tea.KeyMsg:
-		switch messageType.Type {
-		case tea.KeyEnter:
-			model.valueSubmitted = true
-			model.value = model.textInput.Value()
-			return model, tea.Quit
+		switch typeMessage.Type {
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return model, tea.Quit
 		}
 	}
 
-	model.textInput, command = model.textInput.Update(message)
+	if !model.providerIDSubmitted {
+		return updateProviderID(message, model)
+	} else if !model.providerModelSubmitted {
+		return updateProviderModel(message, model)
+	} else {
+		return updateProviderAPIKey(message, model)
+	}
+}
+
+func updateProviderID(message tea.Msg, model Model) (tea.Model, tea.Cmd) {
+	switch typeMessage := message.(type) {
+	case tea.KeyMsg:
+		switch typeMessage.Type {
+		case tea.KeyDown:
+			if model.providerID < 3 {
+				model.providerID++
+			}
+		case tea.KeyUp:
+			if model.providerID > 1 {
+				model.providerID--
+			}
+		case tea.KeyEnter:
+			model.providerIDSubmitted = true
+			return model, nil
+		}
+	}
+	return model, nil
+}
+
+func updateProviderModel(message tea.Msg, model Model) (tea.Model, tea.Cmd) {
+	switch typeMessage := message.(type) {
+	case tea.KeyMsg:
+		switch typeMessage.Type {
+		case tea.KeyDown:
+			if model.providerModel < 3 {
+				model.providerModel++
+			}
+		case tea.KeyUp:
+			if model.providerModel > 1 {
+				model.providerModel--
+			}
+		case tea.KeyEnter:
+			model.providerModelSubmitted = true
+			return model, nil
+		}
+	}
+	return model, nil
+}
+
+func updateProviderAPIKey(message tea.Msg, model Model) (tea.Model, tea.Cmd) {
+	var command tea.Cmd
+
+	switch typeMessage := message.(type) {
+	case tea.KeyMsg:
+		switch typeMessage.Type {
+		case tea.KeyEnter:
+			model.providerAPIKeySubmitted = true
+			model.providerAPIKey = model.providerAPIKeyTextInput.Value()
+			return model, tea.Quit
+		}
+	}
+
+	model.providerAPIKeyTextInput, command = model.providerAPIKeyTextInput.Update(message)
 	return model, command
 }
 
 func (model Model) View() string {
-	inputTextLabel := ConfigKey + "\n\n%s\n\n%s\n"
-	inputTextHelp := "(esc to quit)"
+	if !model.providerIDSubmitted {
+		return providerIDView(model)
+	} else if !model.providerModelSubmitted {
+		return providerModelView(model)
+	} else {
+		return providerAPIKeyView(model)
+	}
+}
 
-	return fmt.Sprintf(inputTextLabel, model.textInput.View(), inputTextHelp)
+func providerIDView(model Model) string {
+	return "provider_id"
+}
+
+func providerModelView(model Model) string {
+	return "provider_model"
+}
+
+func providerAPIKeyView(model Model) string {
+	return "provider_api_key"
 }
 
 var initCommand = &cobra.Command{
@@ -72,9 +148,16 @@ var initCommand = &cobra.Command{
 		}
 
 		programRunModel, programRunOK := programRun.(Model)
-		if programRunOK && programRunModel.valueSubmitted {
-			viper.Set(ConfigKey, programRunModel.value)
-			_, writeConfigError := config.WriteConfig([]string{ConfigKey})
+		if programRunOK && programRunModel.providerAPIKeySubmitted {
+			viper.Set("provider_id", programRunModel.providerID)
+			viper.Set("provider_model", programRunModel.providerModel)
+			viper.Set("provider_api_key", programRunModel.providerAPIKey)
+
+			_, writeConfigError := config.WriteConfig([]string{
+				"provider_id",
+				"provider_model",
+				"provider_api_key",
+			})
 			if writeConfigError != nil {
 				return writeConfigError
 			}
